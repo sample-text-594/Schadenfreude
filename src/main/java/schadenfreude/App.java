a022b36ab6cc10bafbe9a2128622cc7ffa0319e2
FrozenBear98 <FrozenBear98@users.noreply.github.com>
2019-09-26 17:16:00 +0200

    Setup

---
+++ src/main/java/schadenfreude/App.java
@@ -0,0 +1,26 @@
+package schadenfreude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@SpringBootApplication
+@EnableWebSocket
+public class App implements WebSocketConfigurer {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(gameHandler(), "/game");
+	}
+
+	@Bean
+	public WebsocketGameHandler gameHandler() {
+		return new WebsocketGameHandler();
+	}
+}
---
+++ src/main/resources/static/src/states/boot.js
@@ -0,0 +1,29 @@
+var Schadenfreude = {}
+
+Schadenfreude.bootState = function(game) {
+
+}
+
+Schadenfreude.bootState.prototype = {
+
+	init : function() {
+		if (game.global.DEBUG_MODE) {
+			console.log("[DEBUG] Entering **BOOT** state");
+		}
+	},
+
+	preload : function() {
+		this.game.renderer.renderSession.roundPixels = true
+		this.time.desiredFps = game.global.FPS
+	},
+
+	create : function() {
+
+	},
+
+	update : function() {
+		if (typeof game.global.socket !== 'undefined') {
+			//game.state.start('preloadState')
+		}
+	}
+}
